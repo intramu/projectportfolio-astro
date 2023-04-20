488eb54631f09d95f5ef8c89e53ff61a8a9aabb5
Noah Roerig <noahr1936@gmail.com>
2023-04-20 07:11:30 -0700

    Config and path changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://intramu.github.io",
-    base: "/",
+    base: "/projectportfolio-astro",
+    // publicDir: "/projectportfolio-astro",
 });
