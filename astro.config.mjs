4246debd92bf1524b9facd024138123f59df2cab
Noah Roerig <55643662+noahr1936@users.noreply.github.com>
2023-04-21 11:42:04 -0700

    Updated config for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://intramu.github.io",
-    base: "/projectportfolio-astro",
-    // publicDir: "/projectportfolio-astro",
+//     base: "/projectportfolio-astro",
+    base: "/"
 });
